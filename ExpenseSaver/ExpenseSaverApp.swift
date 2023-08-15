2cca3a1b83912658152c41d60f64ecbe2882c8f3
Sandor Suszta <sasha.shusta@gmail.com>
2023-08-15 17:47:17 +0100

    Initial Commit

---
+++ ExpenseSaver/ExpenseSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseSaverApp.swift
+//  ExpenseSaver
+//
+//  Created by Nataliia Shusta on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
